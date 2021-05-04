c17b369efad337639ea9008eec2c99f02df38748
jenerlov <jenerlo@gmail.com>
2021-05-04 11:14:39 +0200

    colors

---
+++ client/src/styling/colorTheme.tsx
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core";
+
+
+export const colorTheme = createMuiTheme( {
+    palette: {
+        primary: {
+            main:"#DCD9F2",
+        },
+        secondary: {
+            main: "#897AF2",
+        }
+    }
+});

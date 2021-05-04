1c8c710ca21e0a7d8259292417c0700bb1a5eab5
jenerlov <jenerlo@gmail.com>
2021-05-04 11:25:04 +0200

    comments

--- client/src/styling/colorTheme.tsx
+++ client/src/styling/colorTheme.tsx
@@ -4,10 +4,10 @@ import { createMuiTheme } from "@material-ui/core";
 export const colorTheme = createMuiTheme({
     palette: {
         primary: {
-            main:"#DCD9F2",
+            main:"#DCD9F2", // Ljuslila
         },
         secondary: {
-            main: "#897AF2",
+            main: "#897AF2", //Mörklila
         }
     }
 });

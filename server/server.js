62ebdb07baf76d63a32e1325eb4b74a0a61f45b7
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-05-04 11:50:07 +0200

    server-setup Co-authored-by: LejlaDolovac <LejlaDolovac@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const server = require('socket.io');
+
+const app = express();
+app.use(express.static('public'));
+
+const server = http.createServer(app);
+const io = new http.Server(server);
+
+io.on('connection', (socket) => {
+	console.log('Client was connected', socket.id);
+
+	// Send to all but socket
+	socket.broadcast.emit('A user connected', socket.id);
+
+	// Send to the user just connected
+	socket.emit('User specific message', 'Welcome to the chat room!');
+
+	//setup event listners
+	socket.on('join-room', (data) => handleJoinRoom(sata, socket));
+	socket.on('disconnect', (reason) => handleDisconnected(reason, socket, io));
+});
+
+server.listen(3000, () => {
+	console.log('Server is running on port 3000');
+});

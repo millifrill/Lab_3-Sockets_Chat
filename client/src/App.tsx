1312bb0c796637bfd57d06484e6cc7eb27ec2790
mariahelenanoren <marialinder97@hotmail.com>
2021-05-05 15:05:17 +0200

    fixed context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,21 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { colorTheme } from './styling/colorTheme';
-import { ThemeProvider } from '@material-ui/core';
-import UserProvider from './contexts/userContext';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { colorTheme } from "./styling/colorTheme";
+import { ThemeProvider } from "@material-ui/core";
+import ChatProvider from "./contexts/chatContext";
 
 function App() {
-	return (
-		<UserProvider>
-			<ThemeProvider theme={colorTheme}>
-				<Router>
-					<Switch>
-						<Route></Route>
-						<Route></Route>
-					</Switch>
-				</Router>
-			</ThemeProvider>
-		</UserProvider>
-	);
+  return (
+    <ChatProvider>
+      <ThemeProvider theme={colorTheme}>
+        <Router>
+          <Switch>
+            <Route></Route>
+            <Route></Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </ChatProvider>
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const http = require('http');
-const server = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const server = http.createServer(app);
-const io = new http.Server(server);
+const io = new Server(server);
 
-io.on('connection', (socket) => {
-	console.log('Client was connected', socket.id);
+io.on("connection", (socket) => {
+  console.log("Client was connected", socket.id);
 
-	// Send to all but socket
-	socket.broadcast.emit('A user connected', socket.id);
+  // Send to all but socket
+  socket.broadcast.emit("A user connected", socket.id);
 
-	// Send to the user just connected
-	socket.emit('User specific message', 'Welcome to the chat room!');
+  // Send to the user just connected
+  socket.emit("User specific message", "Welcome to the chat room!");
 
-	//setup event listners
-	socket.on('join-room', (data) => handleJoinRoom(data, socket));
-	socket.on('disconnect', (reason) => handleDisconnected(reason, socket, io));
+  //setup event listners
+  /* socket.on("join-room", (data) => handleJoinRoom(data, socket));
+  socket.on("disconnect", (reason) => handleDisconnected(reason, socket, io)); */
 });
 
-server.listen(3000, () => {
-	console.log('Server is running on port 3000');
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });

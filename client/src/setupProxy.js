bfe1c82ccc4d0b4fa8dec9a44cb33de12a350aec
mariahelenanoren <marialinder97@hotmail.com>
2021-05-05 10:55:20 +0200

    proxy middleware

---
+++ client/src/setupProxy.js
@@ -0,0 +1,12 @@
+/* const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/socket.io",
+    createProxyMiddleware({
+      target: "ws://localhost:5000/",
+      ws: true,
+      changeOrigin: true,
+    })
+  );
+}; */

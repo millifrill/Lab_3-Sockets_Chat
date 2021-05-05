1549964a05dc3cd74ff830b16e9e515ea3c0413b
jenerlov <jenerlo@gmail.com>
2021-05-05 13:16:37 +0200

    login

---
+++ client/src/routes/loginPage.tsx
@@ -0,0 +1,29 @@
+import { CSSProperties } from "@material-ui/styles"
+
+
+
+export default function loginPage () {
+    return (
+        <div style={mainStyle}>
+            <p style={logo}>Chattastic</p>
+            <div style={loginBox}>
+
+
+            </div>
+        
+
+        </div>
+
+    )
+}
+
+const mainStyle: CSSProperties = {
+
+}   
+
+const logo: CSSProperties = {
+
+}
+const loginBox: CSSProperties = {
+
+}

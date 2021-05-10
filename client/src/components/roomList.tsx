28e105b8e2d6ef6c8df81c67c9fa81c54cb9adc7
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-05-10 07:03:41 +0200

    merge conflicts desolved

--- client/src/components/roomList.tsx
+++ client/src/components/roomList.tsx
@@ -1,60 +1,64 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core';
+import PasswordModal from './passwordModal';
 
 // Logiken för att man ska se vilket rum du är i
 // highligta lila när det är samma rum som inloggad user
 
 export default function RoomList() {
-  const styled = useStyles();
+	const styled = useStyles();
 
-  return (
-    <div className={styled.container}>
-      <div className={styled.chatrooms}>
-        <ol className={styled.olList}>
-          <dt className={styled.roomContainers}>
-            <p className={styled.roomName}>Room 1</p>
-          </dt>
-        </ol>
-        <button className={styled.buttonLogout}>Logout</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styled.container}>
+			<div className={styled.chatrooms}>
+				<ol className={styled.olList}>
+					<dt className={styled.roomContainers}>
+						<p className={styled.roomName}>Room 1</p>
+					</dt>
+					<div>src={PasswordModal}</div>
+				</ol>
+				<button className={styled.buttonLogout}>Logout</button>
+			</div>
+		</div>
+	);
 }
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    display: "flex",
-    boxSizing: "border-box",
-  },
-  chatrooms: {
-    border: "1px solid #F6F6F6",
-    width: "28.9%",
-    height: "100%",
-  },
-
-  olList: {
-    padding: "0",
-    margin: "0",
-  },
-  roomContainers: {
-    border: "1px solid #F6F6F6",
-    height: "4em",
-  },
-
-  buttonLogout: {
-    position: "absolute",
-    bottom: "3%",
-    left: "1rem",
-    borderRadius: "10px",
-    height: "38px",
-    width: "147px",
-    color: "white",
-    background: "#897AF2",
-    border: "none",
-    fontWeight: "bold",
-  },
-
-  roomName: {
-    marginLeft: "2px",
-    fontWeight: "bold",
-  },
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+		},
+	},
+	container: {
+		display: 'flex',
+		boxSizing: 'border-box',
+	},
+	chatrooms: {
+		border: '1px solid #F6F6F6',
+		width: '28.9%',
+		height: '100%',
+	},
+	olList: {
+		padding: '0',
+		margin: '0',
+	},
+	roomContainers: {
+		border: '1px solid #F6F6F6',
+		height: '4em',
+	},
+	buttonLogout: {
+		position: 'absolute',
+		bottom: '3%',
+		left: '1rem',
+		borderRadius: '10px',
+		height: '38px',
+		width: '147px',
+		color: 'white',
+		background: '#897AF2',
+		border: 'none',
+		fontWeight: 'bold',
+	},
+	roomName: {
+		marginLeft: '2px',
+		fontWeight: 'bold',
+	},
 }));

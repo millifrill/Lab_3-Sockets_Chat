3d3207763f1f82f0d3aba55d50098b80b353e0af
mariahelenanoren <marialinder97@hotmail.com>
2021-05-10 13:52:30 +0200

    user must be in a room to view /chatroom

--- client/src/components/chatRoomMain.tsx
+++ client/src/components/chatRoomMain.tsx
@@ -1,38 +1,50 @@
-import { makeStyles } from '@material-ui/core';
-import { useState } from 'react';
-import ChatRoomBox from './chatRoomBox';
-import Header from './header';
-import MobileRoomList from './mobileRoomList';
-import RoomList from './roomList';
+import { makeStyles } from "@material-ui/core";
+import { useState } from "react";
+import ChatRoomBox from "./chatRoomBox";
+import Header from "./header";
+import MobileRoomList from "./mobileRoomList";
+import RoomList from "./roomList";
+import { ChatContext } from "../contexts/chatContext";
+import { useContext, useEffect } from "react";
+import { useHistory } from "react-router";
 
 export default function ChatRoom() {
-	const styled = useStyles();
-	const [mobileRoomList, setMobileRoomList] = useState(false);
+  const history = useHistory();
+  const chatContext = useContext(ChatContext);
+  const { currentRoom } = chatContext;
+  const styled = useStyles();
+  const [mobileRoomList, setMobileRoomList] = useState(false);
 
-	return (
-		<div className={styled.relative}>
-			{mobileRoomList && <MobileRoomList />}
-			<Header
-				setMobileRoomList={setMobileRoomList}
-				mobileRoomList={mobileRoomList}
-			/>
-			<div className={styled.flex}>
-				<RoomList />
-				<ChatRoomBox />
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    if (!currentRoom) {
+      history.push("/");
+    }
+  }, [currentRoom, history]);
+
+  return (
+    <div className={styled.relative}>
+      {mobileRoomList && <MobileRoomList />}
+      <Header
+        setMobileRoomList={setMobileRoomList}
+        mobileRoomList={mobileRoomList}
+      />
+      <div className={styled.flex}>
+        <RoomList />
+        <ChatRoomBox />
+      </div>
+    </div>
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
-	relative: {
-		boxSizing: 'border-box',
-		display: 'flex',
-		flexDirection: 'column',
-	},
-	flex: {
-		boxSizing: 'border-box',
-		display: 'flex',
-		flexDirection: 'row',
-	},
+  relative: {
+    boxSizing: "border-box",
+    display: "flex",
+    flexDirection: "column",
+  },
+  flex: {
+    boxSizing: "border-box",
+    display: "flex",
+    flexDirection: "row",
+  },
 }));

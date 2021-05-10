f5d0c370483ebcdbfaa4f9c88549ceaf361fc4c8
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-05-10 09:53:09 +0200

    commit before pulling main beacuse of new merged branch

--- client/src/components/mobileRoomList.tsx
+++ client/src/components/mobileRoomList.tsx
@@ -1,83 +1,83 @@
-import { Button, makeStyles } from "@material-ui/core";
-import { AddCircle } from "@material-ui/icons";
-import React, { useContext } from "react";
-import { ChatContext } from "../contexts/chatContext";
+import { Button, makeStyles } from '@material-ui/core';
+import { AddCircle } from '@material-ui/icons';
+import { useContext } from 'react';
+import { ChatContext } from '../contexts/chatContext';
 
 export default function MobileRoomList() {
-  const chatContext = useContext(ChatContext);
-  const { allRooms } = chatContext;
-  const styled = useStyles();
-  return (
-    <div className={styled.container}>
-      <div className={styled.listHeader}>
-        <p>Rooms</p>
-        <AddCircle />
-      </div>
-      <div className={styled.roomList}>
-        <ol>
-          {/* Exempelrum */}
-          <dt>Rum 1</dt>
-          {allRooms.map((room) => (
-            <dt>{room.name}</dt>
-          ))}
-        </ol>
-      </div>
-      <div className={styled.buttonContainer}>
-        <Button variant="contained" color="secondary">
-          Logout
-        </Button>
-      </div>
-    </div>
-  );
+	const chatContext = useContext(ChatContext);
+	const { allRooms } = chatContext;
+	const styled = useStyles();
+	return (
+		<div className={styled.container}>
+			<div className={styled.listHeader}>
+				<p>Rooms</p>
+				<AddCircle />
+			</div>
+			<div className={styled.roomList}>
+				<ol>
+					{/* Exempelrum */}
+					<dt>Rum 1</dt>
+					{allRooms.map((room) => (
+						<dt>{room.name}</dt>
+					))}
+				</ol>
+			</div>
+			<div className={styled.buttonContainer}>
+				<Button variant='contained' color='secondary'>
+					Logout
+				</Button>
+			</div>
+		</div>
+	);
 }
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    position: "absolute",
-    display: "flex",
-    flexDirection: "column",
-    top: "5rem",
-    height: "calc(100% - 5rem)",
-    backgroundColor: "#ffff",
-    maxWidth: "100%",
-    width: "18rem",
-    borderRight: "1px solid #E5E5E5",
-    zIndex: 100,
-  },
-  listHeader: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: "1rem 1.5rem",
-    backgroundColor: theme.palette.secondary.main,
-    "& p": {
-      margin: 0,
-      fontWeight: 600,
-      color: "#ffff",
-    },
-    "& svg": {
-      color: "#ffff",
-    },
-  },
-  roomList: {
-    flex: 1,
-    "& ol": {
-      overflowY: "auto",
-      padding: 0,
-      margin: 0,
-      "& dt": {
-        padding: "1rem 1.5rem",
-        margin: 0,
-        borderBottom: "1px solid #E5E5E5",
-      },
-    },
-  },
-  buttonContainer: {
-    padding: "2rem 1.5rem",
-    "& button": {
-      width: "100%",
-      textTransform: "capitalize",
-      fontWeight: 600,
-    },
-  },
+	container: {
+		position: 'absolute',
+		display: 'flex',
+		flexDirection: 'column',
+		top: '5rem',
+		height: 'calc(100% - 5rem)',
+		backgroundColor: '#ffff',
+		maxWidth: '100%',
+		width: '18rem',
+		borderRight: '1px solid #E5E5E5',
+		zIndex: 100,
+	},
+	listHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		padding: '1rem 1.5rem',
+		backgroundColor: theme.palette.secondary.main,
+		'& p': {
+			margin: 0,
+			fontWeight: 600,
+			color: '#ffff',
+		},
+		'& svg': {
+			color: '#ffff',
+		},
+	},
+	roomList: {
+		flex: 1,
+		'& ol': {
+			overflowY: 'auto',
+			padding: 0,
+			margin: 0,
+			'& dt': {
+				padding: '1rem 1.5rem',
+				margin: 0,
+				borderBottom: '1px solid #E5E5E5',
+			},
+		},
+	},
+	buttonContainer: {
+		padding: '2rem 1.5rem',
+		'& button': {
+			width: '100%',
+			textTransform: 'capitalize',
+			fontWeight: 600,
+		},
+	},
 }));
--- client/src/components/roomList.tsx
+++ client/src/components/roomList.tsx
@@ -1,60 +1,165 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core';
+import { AddCircle } from '@material-ui/icons';
+import { useHistory } from 'react-router';
+import { useContext, useEffect, useState } from 'react';
+import { ChatContext } from '../contexts/chatContext';
 
 // Logiken för att man ska se vilket rum du är i
 // highligta lila när det är samma rum som inloggad user
 
 export default function RoomList() {
-  const styled = useStyles();
+	const styled = useStyles();
+	const history = useHistory();
+	const [passwordInput, showPasswordInput] = useState(false);
+	const chatContext = useContext(ChatContext);
+	const { allRooms, errors, currentRoom, handleCreateRoom, handleJoinRoom } =
+		chatContext;
 
-  return (
-    <div className={styled.container}>
-      <div className={styled.chatrooms}>
-        <ol className={styled.olList}>
-          <dt className={styled.roomContainers}>
-            <p className={styled.roomName}>Room 1</p>
-          </dt>
-        </ol>
-        <button className={styled.buttonLogout}>Logout</button>
-      </div>
-    </div>
-  );
+	const [userSettings, setUserSettings] = useState({
+		userName: '',
+		room: {
+			name: '',
+			password: '',
+			isNewRoom: true,
+		},
+	});
+
+	const [userErrors, setUserErrors] = useState({
+		roomName: '',
+	});
+
+	// If user has been assigned a room, redirect to chatroom
+	useEffect(() => {
+		if (currentRoom) {
+			history.push('/chatroom');
+		}
+	}, [history, currentRoom]);
+
+	const handleJoinRoomChange = (index: string) => {
+		const room = allRooms[Number(index)];
+		if (room.hasPassword) {
+			showPasswordInput(true);
+		}
+		setUserSettings((prevState) => ({
+			...prevState,
+			room: {
+				...prevState.room,
+				name: room.name,
+				isNewRoom: false,
+			},
+		}));
+	};
+
+	const handleCreateRoomChange = (name: string) => {
+		if (!name) {
+			setUserErrors((prevState) => ({
+				...prevState,
+				roomName: 'Please enter a room name',
+			}));
+		} else {
+			setUserErrors((prevState) => ({
+				...prevState,
+				roomName: '',
+			}));
+		}
+		setUserSettings((prevState) => ({
+			...prevState,
+			room: {
+				...prevState.room,
+				name: name,
+				isNewRoom: true,
+			},
+		}));
+	};
+
+	const handlePasswordChange = (password: string) => {
+		setUserSettings((prevState) => ({
+			...prevState,
+			room: {
+				...prevState.room,
+				password: password,
+			},
+		}));
+	};
+
+	function checkCreateRoomValidation() {
+		if (!userErrors.roomName && userSettings.room.name) {
+			return true;
+		} else {
+			setUserErrors((prevState) => ({
+				...prevState,
+				roomName: 'Please enter a room name',
+			}));
+			return false;
+		}
+	}
+
+	const newChatRoom = () => {
+		const { password, isNewRoom, name } = userSettings.room;
+		const room = { name: name };
+		if (isNewRoom) {
+			if (checkCreateRoomValidation()) {
+				handleCreateRoom(room, password);
+			}
+		} else {
+			handleJoinRoom(room, password);
+		}
+	};
+
+	return (
+		<div className={styled.container}>
+			<p>Rooms</p>
+			<AddCircle />
+			<div className={styled.chatrooms}>
+				<ol className={styled.olList}>
+					<dt className={styled.roomContainers}>
+						<p className={styled.roomName}>Room 1</p>
+						{allRooms.map((room) => (
+							<dt>{room.name}</dt>
+						))}
+					</dt>
+				</ol>
+				<button className={styled.buttonLogout}>Logout</button>
+			</div>
+		</div>
+	);
 }
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    display: "flex",
-    boxSizing: "border-box",
-  },
-  chatrooms: {
-    border: "1px solid #F6F6F6",
-    width: "28.9%",
-    height: "100%",
-  },
+	container: {
+		display: 'flex',
+		boxSizing: 'border-box',
+	},
+	chatrooms: {
+		border: '1px solid #F6F6F6',
+		width: '28.9%',
+		height: '100%',
+	},
 
-  olList: {
-    padding: "0",
-    margin: "0",
-  },
-  roomContainers: {
-    border: "1px solid #F6F6F6",
-    height: "4em",
-  },
+	olList: {
+		padding: '0',
+		margin: '0',
+	},
+	roomContainers: {
+		border: '1px solid #F6F6F6',
+		height: '4em',
+	},
 
-  buttonLogout: {
-    position: "absolute",
-    bottom: "3%",
-    left: "1rem",
-    borderRadius: "10px",
-    height: "38px",
-    width: "147px",
-    color: "white",
-    background: "#897AF2",
-    border: "none",
-    fontWeight: "bold",
-  },
+	buttonLogout: {
+		position: 'absolute',
+		bottom: '3%',
+		left: '1rem',
+		borderRadius: '10px',
+		height: '38px',
+		width: '147px',
+		color: 'white',
+		background: '#897AF2',
+		border: 'none',
+		fontWeight: 'bold',
+	},
 
-  roomName: {
-    marginLeft: "2px",
-    fontWeight: "bold",
-  },
+	roomName: {
+		marginLeft: '2px',
+		fontWeight: 'bold',
+	},
 }));

922129f877df85af8dad0bc4d199b9625cff1672
Maria Helena Norén <marialinder97@hotmail.com>
2021-12-07 14:39:47 +0100

    fix: fixed socket ports

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
-    transports: ['websocket'],
+const socket = io('http://localhost:4000', {
+  transports: ['websocket'],
 });
 
 export { socket };
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.static('public'));
@@ -11,30 +11,30 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const {
-    handleCreateRoom,
-    handleJoinRoom,
-    handleSendMessage,
-    handleDisconnect,
-    handleLogout,
-    handleRegisterUser,
-    getRooms,
-    handleTyping,
+  handleCreateRoom,
+  handleJoinRoom,
+  handleSendMessage,
+  handleDisconnect,
+  handleLogout,
+  handleRegisterUser,
+  getRooms,
+  handleTyping,
 } = require('./roomEvents');
 
 io.on('connection', (socket) => {
-    console.log('Client was connected', socket.id);
-    io.emit('all-rooms', getRooms(io));
+  console.log('Client was connected', socket.id);
+  io.emit('all-rooms', getRooms(io));
 
-    // Setup event listeners
-    socket.on('join-room', (data) => handleJoinRoom(io, data, socket));
-    socket.on('register-user', (data) => handleRegisterUser(data, socket));
-    socket.on('create-room', (data) => handleCreateRoom(data, socket, io));
-    socket.on('send-message', (data) => handleSendMessage(data, io, socket));
-    socket.on('typing', (data) => handleTyping(data, socket));
-    socket.on('logout', (data) => handleLogout(data, socket, io));
-    socket.on('disconnect', (reason) => handleDisconnect(reason, io, socket));
+  // Setup event listeners
+  socket.on('join-room', (data) => handleJoinRoom(io, data, socket));
+  socket.on('register-user', (data) => handleRegisterUser(data, socket));
+  socket.on('create-room', (data) => handleCreateRoom(data, socket, io));
+  socket.on('send-message', (data) => handleSendMessage(data, io, socket));
+  socket.on('typing', (data) => handleTyping(data, socket));
+  socket.on('logout', (data) => handleLogout(data, socket, io));
+  socket.on('disconnect', (reason) => handleDisconnect(reason, io, socket));
 });
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
